ab846f70a32350185a3f111b1c9666fd964d49f9
ukaratkevich <vkorotkevich@damsols.com>
2023-06-01 21:33:34 +0200

    change style

--- hello-compose/src/main/java/dev/karatkevich/mobile/showcase/hello/compose/MainScreen.kt
+++ hello-compose/src/main/java/dev/karatkevich/mobile/showcase/hello/compose/MainScreen.kt
@@ -3,10 +3,10 @@ package dev.karatkevich.mobile.showcase.hello.compose
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,6 +20,6 @@ fun MainScreen() {
             .padding(8.dp)
             .wrapContentHeight(),
         textAlign = TextAlign.Center,
-        color = Color.White
+        style = MaterialTheme.typography.titleLarge
     )
 }
