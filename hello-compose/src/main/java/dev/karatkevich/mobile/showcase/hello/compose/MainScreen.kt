863606c61b1309ffef5ebab1f77c27dae4dec33b
ukaratkevich <vkorotkevich@damsols.com>
2023-06-03 12:37:05 +0200

    wraped text in the box and separated the preview

--- hello-compose/src/main/java/dev/karatkevich/mobile/showcase/hello/compose/MainScreen.kt
+++ hello-compose/src/main/java/dev/karatkevich/mobile/showcase/hello/compose/MainScreen.kt
@@ -1,25 +1,39 @@
 package dev.karatkevich.mobile.showcase.hello.compose
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-@Preview
-fun MainScreen() {
-    Text(
-        text = "Hello Compose!",
-        modifier = Modifier.fillMaxSize()
-            .padding(8.dp)
-            .wrapContentHeight(),
-        textAlign = TextAlign.Center,
-        style = MaterialTheme.typography.titleLarge
-    )
+fun MainScreen(message: String) {
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = message,
+            modifier = Modifier.fillMaxWidth()
+                .padding(8.dp),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.titleLarge,
+        )
+    }
 }
+
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = "id:pixel_4a",
+)
+private fun MainScreenPreview() {
+    MainScreen("Hello Compose!")
+}
+
